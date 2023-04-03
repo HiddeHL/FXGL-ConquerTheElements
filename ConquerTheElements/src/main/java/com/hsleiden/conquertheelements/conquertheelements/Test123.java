5f3f0fe8eed37f5b5c0ab1f9595bb2be2836dc79
karim <vpowerfate123@gmail.com>
2023-04-03 13:59:28 +0200

    test of ik dit bestaan in mijn eigen branch kan zetten.

---
+++ ConquerTheElements/src/main/java/com/hsleiden/conquertheelements/conquertheelements/Test123.java
@@ -0,0 +1,7 @@
+package com.hsleiden.conquertheelements.conquertheelements;
+
+public class Test123 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
